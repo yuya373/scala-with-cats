e1e8816ad42bd182a46cca0251806b8a0860eba8
Yuya Minami <yuya373@me.com>
2017-12-31 00:10:25 +0900

    3.6.1 contramap

--- cats-sandbox/src/main/scala/sandbox/Printable.scala
+++ cats-sandbox/src/main/scala/sandbox/Printable.scala
@@ -1,11 +1,20 @@
 trait Printable[A] {
+  // use a `self` alias to distinguish the outer and inner Printables
+  self =>
   def format(value: A): String
   def print(value: A): Unit = println(format(value))
+  def contramap[B](f: B => A): Printable[B] = new Printable[B] {
+    def format(value: B): String = {
+      self.format(f(value))
+    }
+  }
 }
+
+final case class Box[A](value: A)
 
 object PrintableInstances {
   implicit val stringPrinter: Printable[String] = new Printable[String] {
-    def format(value: String) = value
+    def format(value: String) = s""""${value}""""
   }
 
   implicit val intPrinter: Printable[Int] = new Printable[Int] {
@@ -16,6 +25,17 @@ object PrintableInstances {
     def format(value: Cat) =
       s"${value.name} is a ${value.age} year-old ${value.color} cat."
   }
+
+  implicit val booleanPrinter: Printable[Boolean] = new Printable[Boolean] {
+    def format(value: Boolean) = if (value) "yes" else "no"
+  }
+
+  implicit def boxPrinter[A](implicit p: Printable[A]): Printable[Box[A]] =
+    // F[B].contramap(A => B): F[A]
+    // Printable[A].contramap(Box[A] => A): Printable[Box[A]]
+    // transformation from Printable[A] to Printable[Box[A]]
+    // prepending (f: Box[A] => A) to Printable[A]
+    p.contramap((box: Box[A]) => box.value)
 }
 
 object Printable {
