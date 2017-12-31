32182401f6cc995d46dae4c6ba7befe232469a71
Yuya Minami <yuya373@me.com>
2017-12-31 22:59:38 +0900

    chapter 4.3

--- cats-sandbox/src/main/scala/sandbox/Chapter4.scala
+++ cats-sandbox/src/main/scala/sandbox/Chapter4.scala
@@ -1,3 +1,8 @@
+import cats.{ Monad => CMonad }
+import cats.syntax.functor._
+import cats.syntax.flatMap._
+import cats.Id
+
 object Chapter4 {
   def parseInt(str: String): Option[Int] =
     scala.util.Try(str.toInt).toOption
@@ -16,4 +21,13 @@ object Chapter4 {
     def map[A, B](fa: F[A])(f: A => B): F[B] =
       flatmap(fa)((a: A) => pure(f(a)))
   }
+  // Scala cannot unify types and type constructors when searching for implicits
+  // Hence our need to re-type Int as Id[Int]
+  // sumSquare(3: Id[Int], 4: Id[Int])
+  def sumSquare[F[_]: CMonad](a: F[Int], b: F[Int]): F[Int] =
+    a.flatMap(x => b.map(y => x * x + y * y))
+
+  def pure[A](a: A): Id[A] = a
+  def map[A, B](fa: Id[A])(f: A => B): Id[B] = f(fa)
+  def flatMap[A, B](fa: Id[A])(f: A => Id[B]): Id[B] = f(fa)
 }
